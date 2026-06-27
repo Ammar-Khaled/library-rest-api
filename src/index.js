b69c0ec53544c9044a280eb3473b579424a7e6c4
Ammar_Khaled <ammarkhaled2003@gmail.com>
2026-06-27 14:42:42 +0300

    integrate swagger-ui-express for documentation

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use('/authors', authorsRouter);
 app.use('/books',   booksRouter);
 app.use('/loans',   loansRouter);
